ec81fac29ff9e922cc1aa7e1000fc7fc5a61e973
Timothy <timiancam@gmail.com>
2023-10-29 14:11:46 +0000

    Add variable

--- connection_handler.py
+++ connection_handler.py
@@ -8,17 +8,19 @@ Accessed by other modules
 import psycopg2 as ps2
 from sqlalchemy import create_engine
 
+DATABASE_URL = 'postgresql+psycopg2://::@localhost:5432/postgres'
+
 # connect to the database
 try:
-    ENGINE = create_engine('postgresql+psycopg2://::@localhost:5432/postgres')
+    ENGINE = create_engine(DATABASE_URL)
     CONN = ENGINE.raw_connection()
     CONN.set_session(autocommit=True)
     CUR = CONN.cursor()
 except ps2.Error as e:
-    print('Error connecting to database.')
+    print('Error connecting to database')
     print(e)
 
 def database_disconnect():
     CUR.close()
     CONN.close()
-    print('Disconnected from database.\n')
+    print('Disconnected from database\n')
